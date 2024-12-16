38aeab8073a2f544e1d40818eb40fb5c09829a09
stumpa01 <stumpa@student.ethz.ch>
2024-12-16 17:43:51 +0100

    Ensure the window parameters are set correctly when using write_uwreal

--- src/IO.jl
+++ src/IO.jl
@@ -1,22 +1,25 @@
 """
-    read_uwreal(file; return_info=false) -> A::Union{AD.uwreal, AbstractArray{AD.uwreal} [, info::String]
+    read_uwreal(filename; return_info=false) -> a::Union{AD.uwreal, AbstractArray{AD.uwreal} [, info::String]
 
-Read the `AD.uwreal` objects stored in `file` and return them as an array with the
-specified shape. If `length(A)==1` return it as a single object.
-If `return_info=true`, additionally return the `info` string containing the creaton
-data and program versions.
+Read `AD.uwreal` objects from a BDIO file and return them as an array with the shape
+specified in the file. If `length(a)==1` return it as a single object. The window
+parameters are set in `parms.wpm` accordingly.
+If `return_info=true`, additionally return the `info` string containing the creation
+date and program versions.
 
 ## Note
 The file is assumed to have the shape specified in the doc of the `write_uwreal`
 function.
 """
-function read_uwreal(file; return_info=false)
+function read_uwreal(filename; return_info=false)
     # Open BDIO file
-    fb = BDIO.BDIO_open(string(file), "r")
+    fb = BDIO.BDIO_open(string(filename), "r")
 
-    # Read info and size of stored array
+    # Read info, size of stored array, ensemble ids and window parameters
     info = nothing
     dims = nothing
+    ids = nothing
+    wpm = nothing
     while BDIO.BDIO_seek!(fb) && (info === nothing || dims === nothing)
         # Read info
         if BDIO.BDIO_get_uinfo(fb) == 0
@@ -27,11 +30,24 @@ function read_uwreal(file; return_info=false)
         if BDIO.BDIO_get_uinfo(fb) == 1
             # Read number of dimensions
             ndims = Vector{Int64}(undef, 1)
-            BDIO.BDIO_read(fb, ndims, 1)
+            BDIO.BDIO_read(fb, ndims)
 
             # Read size of array
             dims = Vector{Int64}(undef, ndims[1])
             BDIO.BDIO_read(fb, dims)
+
+            # Read ensemble ids and window parameters
+            nids = Vector{Int64}(undef, 1)
+            BDIO.BDIO_read(fb, nids)
+            if nids[1] > 0
+                ids = Vector{Int64}(undef, nids[1])
+                BDIO.BDIO_read(fb, ids)
+                wpm = Vector{Float64}(undef, 4*nids[1])
+                BDIO.BDIO_read(fb, wpm)
+            else
+                ids = Int[]
+                wpm = Float64[]
+            end
         end
     end
     if info === nothing || dims === nothing
@@ -41,11 +57,11 @@ function read_uwreal(file; return_info=false)
     # Read uwreal objects
     len = prod(dims)
     idx = 0
-    A = Vector{AD.uwreal}(undef, len)
+    a = Vector{AD.uwreal}(undef, len)
     while BDIO.BDIO_seek!(fb) && idx < len
         if BDIO.BDIO_get_uinfo(fb) == 2
             idx += 1
-            A[idx] = AD.read_uwreal(fb)
+            a[idx] = AD.read_uwreal(fb)
         end
     end
     if idx != len
@@ -55,27 +71,34 @@ function read_uwreal(file; return_info=false)
     BDIO.BDIO_close!(fb)
 
     # Reshape array
-    A = reshape(A, dims...)
-    if length(A) == 1
-        A = A[1]
+    a = reshape(a, dims...)
+    if length(a) == 1
+        a = a[1]
+    end
+
+    # Set window parameters
+    wpm = reshape(wpm, 4, :)
+    for (i, id) in enumerate(ids)
+        ensemble = AD.get_name_from_id(id, AD.wsg)
+        parms.wpm[ensemble] = wpm[:, i]
     end
 
     if return_info
-        return A, info
+        return a, info
     else
-        return A
+        return a
     end
 end
 
 """
-    write_uwreal(file, a::AD.uwreal, mode="d")
-    write_uwreal(file, a::AbstractArray{AD.uwreal}, mode="d")
+    write_uwreal(filename, a::AD.uwreal, mode="d")
+    write_uwreal(filename, a::AbstractArray{AD.uwreal}, mode="d")
 
-Write the `AD.uwreal` or array of `AD.uwreal` `a` to the BDIO `file`. Specify how the file
-is opend with `mode` (default is "d").
+Write the `AD.uwreal` or array of `AD.uwreal` `a` to a BDIO file. Specify how the file is
+opend with `mode` (default is "d").
 
 Additionally, write an info string containing the creation date and program version together
-with the shape of the array to the file.
+with the shape of the array and the relevant window parameters in `parms.wpm` to the file.
 
 ### Possible modes
 - Write mode ("w"): The file is created. If the file exists an error is
@@ -84,22 +107,25 @@ with the shape of the array to the file.
   overwritten.
 
 ### Format of file
-The info string, the shape and the `AD.uwreal` are stored to different records in the
-following way:
+The info string, the shape, the window parameters and the `AD.uwreal` are stored to
+different records in the following way:
 - Record 0: Info string as `BDIO_ASC_GENERIC`.
-- Record 1: [ndims(a), size(a)...]  as `BDIO_BIN_INT64LE` (or [1, 1] if it's just a single
+- Record 1: ndims(a), size(a)..., length(ids), ids..., wps... as `BDIO_BIN_GENERIC` where
+    `ids::Vector{Int}` are the ensemble ids and `wps::Vector{Float64}` are the (flattened)
+    window parameters relevent for `a`.
+    (or set ndims(a) = 1 size(a) = 1 if it's just a single
   `AD.uwreal`).
 - Record 2: The `AD.uwreal` are stored in consecutive records 2 using the function
   `AD.write_uwreal`.
 """
-function write_uwreal(file, a::AbstractArray{AD.uwreal}, mode="d")
+function write_uwreal(filename, a::AbstractArray{AD.uwreal}, mode="d")
     # Check that the mode is valid
     if mode ∉ ["w", "d"]
         throw(ArgumentError("mode must be 'w' or 'd'."))
     end
 
     # Create a BDIO file
-    fb = BDIO.BDIO_open(string(file), mode, "uwreal")
+    fb = BDIO.BDIO_open(string(filename), mode, "uwreal")
 
     # Write info string to record 0
     info = 
@@ -109,21 +135,45 @@ function write_uwreal(file, a::AbstractArray{AD.uwreal}, mode="d")
 
     BDIO.BDIO_start_record!(fb, BDIO.BDIO_ASC_GENERIC, 0)
     BDIO.BDIO_write!(fb, info)
+
     BDIO.BDIO_write_hash!(fb)
 
-    # Write size of array to record 1
-    BDIO.BDIO_start_record!(fb, BDIO.BDIO_BIN_INT64LE, 1)
-    BDIO.BDIO_write!(fb, [ndims(a), size(a)...])
+    # Get all ensemble ids for `a` that are in parms.wpm and their window parameters 
+    AD.unique_ids!.(a, [AD.wsg])
+    ids = Int[]
+    wpm = Float64[]
+    for v in a
+        for id in v.ids
+            ensemble = AD.get_name_from_id(id, AD.wsg)
+            if id ∉ ids && haskey(parms.wpm, ensemble)
+                push!(ids, id)
+                append!(wpm, parms.wpm[ensemble])
+            end
+        end
+    end
+
+    # Start writing to record 1
+    BDIO.BDIO_start_record!(fb, BDIO.BDIO_BIN_GENERIC, 1)
+    
+    # Write size of array and number of ensembles
+    BDIO.BDIO_write!(fb, Int[ndims(a), size(a)..., length(ids)])
+
+    # Write the ensemble ids and then their window parameters
+    if length(ids) > 0
+        BDIO.BDIO_write!(fb, ids)
+        BDIO.BDIO_write!(fb, wpm)
+    end
+
     BDIO.BDIO_write_hash!(fb)
 
-    # Write the uwreal objects in `A` to record 2
+    # Write the uwreal objects in `a` to record 2
     for v in a
         AD.write_uwreal(v, fb, 2)
     end
 
     BDIO.BDIO_close!(fb)
 end
-write_uwreal(file, a::AD.uwreal, mode="d") = write_uwreal(file, [a], mode)
+write_uwreal(filename, a::AD.uwreal, mode="d") = write_uwreal(filename, [a], mode)
 
 """
     export_samples(a::AD.uwreal) -> samples
